169101f883c4aaeded02c469c545890f231162b1
mrustamov <rustamov.90@gmail.com>
2020-05-10 01:37:59 +0200

    tut completed

--- scripts/index.js
+++ scripts/index.js
@@ -3,6 +3,7 @@ const API_URL = `https://api.nasa.gov/insight_weather/?api_key=${API_KEY}&feedty
 
 const previousWeatherToggle = document.querySelector(".show-previous-weather");
 const previousWeather = document.querySelector(".previous-weather");
+
 const currentSolElement = document.querySelector("[data-current-sol]");
 const currentDateElement = document.querySelector("[data-current-date]");
 const currentTempHighElement = document.querySelector(
@@ -15,18 +16,51 @@ const windDirectionArrow = document.querySelector(
   "[data-wind-direction-arrow]"
 );
 
+const previousSolTemplate = document.querySelector(
+  "[data-previous-sol-template]"
+);
+const previousSolContainer = document.querySelector("[data-previous-sols]");
+
+const unitToggle = document.querySelector("[data-unit-toggle]");
+const metricRadio = document.getElementById("cel");
+const imperialRadio = document.getElementById("fah");
+
 previousWeatherToggle.addEventListener("click", () => {
   previousWeather.classList.toggle("show-weather");
 });
+
 let selectedSolIndex;
+
 getWeather().then((sols) => {
   selectedSolIndex = sols.length - 1;
   displaySelectedSol(sols);
+  displayPreviousSols(sols);
+  updateUnits();
+
+  unitToggle.addEventListener("click", () => {
+    let metricUnits = !isMetric();
+    metricRadio.checked = metricUnits;
+    imperialRadio.checked = !metricUnits;
+    displaySelectedSol(sols);
+    displayPreviousSols(sols);
+    updateUnits();
+  });
+
+  metricRadio.addEventListener("change", () => {
+    displaySelectedSol(sols);
+    displayPreviousSols(sols);
+    updateUnits();
+  });
+
+  imperialRadio.addEventListener("change", () => {
+    displaySelectedSol(sols);
+    displayPreviousSols(sols);
+    updateUnits();
+  });
 });
 
 function displaySelectedSol(sols) {
   const selectedSol = sols[selectedSolIndex];
-  console.log(selectedSol);
   currentSolElement.innerText = selectedSol.sol;
   currentDateElement.innerText = displayDate(selectedSol.date);
   currentTempHighElement.innerText = displayTemperature(selectedSol.maxTemp);
@@ -39,16 +73,48 @@ function displaySelectedSol(sols) {
   windDirectionText.innerText = selectedSol.windDirectionCardinal;
 }
 
+function displayPreviousSols(sols) {
+  previousSolContainer.innerText = "";
+  sols.forEach((solData, index) => {
+    const solContainer = previousSolTemplate.content.cloneNode(true);
+    solContainer.querySelector("[data-sol]").innerText = solData.sol;
+    solContainer.querySelector("[data-date]").innerText = displayDate(
+      solData.date
+    );
+    solContainer.querySelector(
+      "[data-temp-high]"
+    ).innerText = displayTemperature(solData.maxTemp);
+    solContainer.querySelector(
+      "[data-temp-low]"
+    ).innerText = displayTemperature(solData.minTemp);
+    solContainer
+      .querySelector("[data-select-button]")
+      .addEventListener("click", () => {
+        selectedSolIndex = index;
+        displaySelectedSol(sols);
+      });
+    previousSolContainer.appendChild(solContainer);
+  });
+}
+
 function displayDate(date) {
   return date.toLocaleDateString(undefined, { day: "numeric", month: "long" });
 }
 
 function displayTemperature(temperature) {
-  return Math.round(temperature);
+  let returnTemp = temperature;
+  if (!isMetric()) {
+    returnTemp = temperature * (9 / 5) + 32;
+  }
+  return Math.round(returnTemp);
 }
 
 function displaySpeed(speed) {
-  return Math.round(speed);
+  let returnSpeed = speed;
+  if (!isMetric()) {
+    returnSpeed = speed / 1.609;
+  }
+  return Math.round(returnSpeed);
 }
 
 function getWeather() {
@@ -68,4 +134,21 @@ function getWeather() {
         };
       });
     });
+}
+
+function updateUnits() {
+  const speedUnits = document.querySelectorAll("[data-speed-unit]");
+  const tempUnits = document.querySelectorAll("[data-temp-unit]");
+
+  speedUnits.forEach((unit) => {
+    unit.innerText = isMetric() ? "kph" : "mph";
+  });
+
+  tempUnits.forEach((unit) => {
+    unit.innerText = isMetric() ? "C" : "F";
+  });
+}
+
+function isMetric() {
+  return metricRadio.checked;
 }
