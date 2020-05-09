3f77941857ffae37006c4cd6aed272f8f2efe09d
mrustamov <rustamov.90@gmail.com>
2020-05-09 22:46:07 +0200

    first update

--- scripts/index.js
+++ scripts/index.js
@@ -1,11 +1,71 @@
-// Where the cool stuff happens
+const API_KEY = "DEMO_KEY";
+const API_URL = `https://api.nasa.gov/insight_weather/?api_key=${API_KEY}&feedtype=json&ver=1.0`;
 
+const previousWeatherToggle = document.querySelector(".show-previous-weather");
+const previousWeather = document.querySelector(".previous-weather");
+const currentSolElement = document.querySelector("[data-current-sol]");
+const currentDateElement = document.querySelector("[data-current-date]");
+const currentTempHighElement = document.querySelector(
+  "[data-current-temp-high]"
+);
+const currentTempLowElement = document.querySelector("[data-current-temp-low]");
+const windSpeedElement = document.querySelector("[data-wind-speed]");
+const windDirectionText = document.querySelector("[data-wind-direction-text]");
+const windDirectionArrow = document.querySelector(
+  "[data-wind-direction-arrow]"
+);
 
-// Change this if you need to
-const previousWeatherToggle = document.querySelector('.show-previous-weather');
+previousWeatherToggle.addEventListener("click", () => {
+  previousWeather.classList.toggle("show-weather");
+});
+let selectedSolIndex;
+getWeather().then((sols) => {
+  selectedSolIndex = sols.length - 1;
+  displaySelectedSol(sols);
+});
 
-const previousWeather = document.querySelector('.previous-weather')
+function displaySelectedSol(sols) {
+  const selectedSol = sols[selectedSolIndex];
+  console.log(selectedSol);
+  currentSolElement.innerText = selectedSol.sol;
+  currentDateElement.innerText = displayDate(selectedSol.date);
+  currentTempHighElement.innerText = displayTemperature(selectedSol.maxTemp);
+  currentTempLowElement.innerText = displayTemperature(selectedSol.minTemp);
+  windSpeedElement.innerText = displaySpeed(selectedSol.windSpeed);
+  windDirectionArrow.style.setProperty(
+    "--direction",
+    `${selectedSol.windDirectionDegrees}deg`
+  );
+  windDirectionText.innerText = selectedSol.windDirectionCardinal;
+}
 
-previousWeatherToggle.addEventListener('click', () => {
-    previousWeather.classList.toggle('show-weather')
-} )
+function displayDate(date) {
+  return date.toLocaleDateString(undefined, { day: "numeric", month: "long" });
+}
+
+function displayTemperature(temperature) {
+  return Math.round(temperature);
+}
+
+function displaySpeed(speed) {
+  return Math.round(speed);
+}
+
+function getWeather() {
+  return fetch(API_URL)
+    .then((res) => res.json())
+    .then((data) => {
+      const { sol_keys, validity_checks, ...solData } = data;
+      return Object.entries(solData).map(([sol, data]) => {
+        return {
+          sol: sol,
+          maxTemp: data.AT.mx,
+          minTemp: data.AT.mn,
+          windSpeed: data.HWS.av,
+          windDirectionDegrees: data.WD.most_common.compass_degrees,
+          windDirectionCardinal: data.WD.most_common.compass_point,
+          date: new Date(data.First_UTC),
+        };
+      });
+    });
+}
